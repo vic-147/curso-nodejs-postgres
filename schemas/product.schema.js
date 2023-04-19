ab34efe6834d9bfb863b3819bc2e94660f29b0ca
vic-147 <dmkorps@gmail.com>
2023-04-19 13:08:31 -0400

    :sparkles: paginacion y rango de precio
    yuyu

--- schemas/product.schema.js
+++ schemas/product.schema.js
@@ -6,6 +6,12 @@ const price = Joi.number().integer().min(10);
 const description = Joi.string().min(10);
 const image = Joi.string().uri();
 const categoryId = Joi.number().integer();
+// paginacion
+const limit = Joi.number().integer();
+const offset = Joi.number().integer();
+// rango de precio
+const price_min = Joi.number().integer();
+const price_max = Joi.number().integer();
 
 const createProductSchema = Joi.object({
   name: name.required(),
@@ -20,11 +26,27 @@ const updateProductSchema = Joi.object({
   price: price,
   description: description,
   image: image,
-  categoryId
+  categoryId,
 });
 
 const getProductSchema = Joi.object({
   id: id.required(),
 });
 
-module.exports = { createProductSchema, updateProductSchema, getProductSchema };
+const queryProductSchema = Joi.object({
+  limit,
+  offset,
+  price,
+  price_min,
+  price_max: price_max.when('price_min', {
+    is: Joi.number().integer(),
+    then: Joi.required(),
+  }),
+});
+
+module.exports = {
+  createProductSchema,
+  updateProductSchema,
+  getProductSchema,
+  queryProductSchema,
+};
--- services/product.service.js
+++ services/product.service.js
@@ -1,6 +1,7 @@
 const { faker } = require('@faker-js/faker');
 const boom = require('@hapi/boom');
 const { models } = require('../libs/sequelize');
+const { Op } = require('sequelize');
 
 class ProductsService {
   constructor() {
@@ -26,10 +27,30 @@ class ProductsService {
     return products;
   }
 
-  async find() {
-    const products = await models.Product.findAll({
+  // paginacion opcional
+  async find(query) {
+    const options = {
       include: ['category'],
-    });
+      where: {},
+    };
+    const { limit, offset } = query;
+    if (limit && offset) {
+      (options.limit = limit), (options.offset = offset);
+    }
+    //filtrado por precio
+    const { price } = query;
+    if (price) {
+      options.where.price = price;
+    }
+    //rango de precio
+    const { price_min, price_max } = query;
+    if (price_min && price_max) {
+      options.where.price = {
+        [Op.gte]: price_min,
+        [Op.lte]: price_max,
+      };
+    }
+    const products = await models.Product.findAll(options);
     return products;
   }
 
